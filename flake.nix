2768cf6de957d62572488d23f4f785eeedb5a99d
damianfral <huevofritopamojarpan@gmail.com>
2022-12-26 04:30:33 +0000

    Update flake.lock .

--- flake.nix
+++ flake.nix
@@ -56,6 +56,7 @@
                 (self: super: {
                   autodocodec-yaml = final.haskell.lib.unmarkBroken super.autodocodec-yaml;
                   github-ls = self.callCabal2nix "github-ls" ./. { };
+                  sydtest = with final.haskell.lib; unmarkBroken (dontCheck super.sydtest);
                 }
                 );
             });
