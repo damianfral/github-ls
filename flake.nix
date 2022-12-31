e683c8d327641f47088b163feb4c24f8e0fa4a9f
damianfral <huevofritopamojarpan@gmail.com>
2022-12-31 01:40:55 +0000

    Use generateOptparseApplicativeCompletions.

--- flake.nix
+++ flake.nix
@@ -19,49 +19,52 @@
       };
     in
 
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        rec {
-          packages = {
-            github-ls = pkgs.haskellPackages.github-ls.overrideAttrs (oldAttrs: {
-              nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-              postInstall = ''
-                wrapProgram $out/bin/github-ls \
-                  --suffix PATH : ${pkgs.lib.makeBinPath [ pkgs.github-cli ]}
-              '';
-            }
-            );
-          };
-
-          defaultPackage = pkgs.haskell.lib.justStaticExecutables
-            packages.github-ls;
-
-          devShells.default = pkgs.haskellPackages.shellFor {
-            packages = p: [ packages.github-ls ];
-            buildInputs = with pkgs; with pkgs.haskellPackages; [
-              haskell-language-server
-              cabal-install
-              ghcid
-              hpack
-              hlint
-              yamlfix
-            ];
-          };
-
-          overlays = final: prev: {
-            haskellPackages = prev.haskellPackages.override (old: {
-              overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
-                (self: super: {
-                  autodocodec-yaml = final.haskell.lib.unmarkBroken super.autodocodec-yaml;
-                  github-ls = self.callCabal2nix "github-ls" ./. { };
-                  sydtest = with final.haskell.lib; unmarkBroken (dontCheck super.sydtest);
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = pkgsFor system;
+      in
+      rec {
+        packages = {
+          github-ls =
+            pkgs.haskell.lib.justStaticExecutables (
+              pkgs.haskellPackages.github-ls.overrideAttrs
+                (oldAttrs: {
+                  nativeBuildInputs = oldAttrs.nativeBuildInputs
+                    ++ [ pkgs.makeWrapper ];
+                  postInstall = (oldAttrs.postInstall or "") + ''
+                    wrapProgram $out/bin/github-ls \
+                      --suffix PATH : ${pkgs.lib.makeBinPath [pkgs.github-cli]}
+                  '';
                 }
-                );
-            });
-          };
-        });
+                ));
+        };
+
+        defaultPackage = packages.github-ls;
+
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [ packages.github-ls ];
+          buildInputs = with pkgs; with pkgs.haskellPackages; [
+            haskell-language-server
+            cabal-install
+            ghcid
+            hpack
+            hlint
+            yamlfix
+          ];
+        };
+
+        overlays = final: prev: with final.haskell.lib; {
+          haskellPackages = prev.haskellPackages.override (old: {
+            overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
+              (self: super: {
+                autodocodec-yaml = unmarkBroken super.autodocodec-yaml;
+                github-ls = self.generateOptparseApplicativeCompletions
+                  [ "github-ls" ]
+                  (self.callCabal2nix "github-ls" ./. { });
+                sydtest = unmarkBroken (dontCheck super.sydtest);
+              }
+              );
+          });
+        };
+      });
 }
 
